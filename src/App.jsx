92b80db39e3fdf44ebe6e68e688fcbf4ad973d6e
greyGroot <grey.rsm@gmail.com>
2021-02-01 15:44:47 +0200

    add agenda

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import { Deck, Slide, FlexBox, Box, FullScreen, Progress } from 'spectacle';
-
-import logo from './logo.svg';
+import { Deck, Slide, FlexBox, Box, FullScreen, Progress, Heading, Text, UnorderedList, ListItem, CodeSpan, Notes } from 'spectacle';
 
 const theme = {
   fonts: {
@@ -29,8 +27,46 @@ const template = () => (
 const App = () => (
   <Deck theme={theme} template={template} transitionEffect="fade">
     <Slide>
-      <FlexBox>
-        <img src={logo} alt="logo" height='100' width='100' />
+      <FlexBox height="100%" flexDirection="column">
+        <Heading>
+          React Renders Optimization
+        </Heading>
+        <Text textSize="1.7em" margin="40px 0px 0px" bold textColor="tertiary">
+          Sergii Rumiantsev
+        </Text>
+        <Text textSize="1.2em" margin="40px 0px 0px" bold textColor="#4caf50">
+          Levi9
+        </Text>
+      </FlexBox>
+    </Slide>
+    <Slide>
+      <FlexBox height="100%" flexDirection="column">
+      <Heading>Agenda</Heading>
+      <UnorderedList>
+        <ListItem>
+          <CodeSpan>Pre-optimize or not to pre-optimize?</CodeSpan>
+        </ListItem>
+        <ListItem>
+          <CodeSpan>Props: &#123;&#123;styles&#125;&#125;, functions and big objects</CodeSpan>
+        </ListItem>
+        <ListItem>
+          <CodeSpan>Conditional rendering</CodeSpan>
+        </ListItem>
+        <ListItem>
+          <CodeSpan>useMemo / useCallback / useSuperMemo</CodeSpan>
+        </ListItem>
+        <ListItem>
+          <CodeSpan>React.memo / ShouldComponentUpdate</CodeSpan>
+        </ListItem>
+        <ListItem><CodeSpan>React.Context</CodeSpan></ListItem>
+        <ListItem>
+          <CodeSpan>So lets React.memo everything?</CodeSpan>
+        </ListItem>
+      </UnorderedList>
+      </FlexBox>
+    </Slide>
+    <Slide>
+      <FlexBox height="100%" flexDirection="column">
       </FlexBox>
     </Slide>
   </Deck>
